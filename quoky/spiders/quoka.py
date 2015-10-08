1f65b633bba8f781239346e471c914415f8e2e37
Daniel Gonzalez (polyphemus) <daniel.gonzalez.valero@gmail.com>
2015-10-08 12:54:52 +0200

    Walk visible cities

--- quoky/spiders/quoka.py
+++ quoky/spiders/quoka.py
@@ -9,4 +9,8 @@ class QuokySpider(scrapy.Spider):
     start_urls = ['http://www.quoka.de/immobilien/bueros-gewerbeflaechen']
 
     def parse(self, response):
+        for url in response.css('body > div.spr-wrp > div.cnv > div.cnt > aside > form > div:nth-child(5) > div > ul > li > ul > li > a').xpath('@href').extract():
+            yield scrapy.Request(response.urljoin(url), self.city_pager)
+
+    def city_pager(self, response):
         pass
